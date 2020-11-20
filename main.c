b43d135e066330eddf5043f2b44e64eb7d43e41b
AZMCode <adrianozambrana@protonmail.com>
2020-11-20 15:20:18 -0400

    Added voice line after disguising

--- main.c
+++ main.c
@@ -4,8 +4,7 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/random.h>
-#define DISGUISE_PATTERN "disguise 0 -1\n"
-#define DISGUISE_PATTERN_SIZE 15
+#define DISGUISE_PATTERN "disguise 0 -1\nvoicemenu 2 1"
 
 
 MODULE_LICENSE("GPL");
@@ -14,20 +13,19 @@ MODULE_DESCRIPTION("TF2 Random disguise device");
 MODULE_VERSION("0.0.1");
 
 ssize_t read_disguise(struct file* file_reading, char __user* user_buffer, size_t size_to_read, loff_t* offset){
-    if(*offset >= DISGUISE_PATTERN_SIZE){
+    if(*offset >= strlen(DISGUISE_PATTERN)){
         return 0;
     } else {
         int i;
         int readedBytes;
-        for(i=*offset;i<DISGUISE_PATTERN_SIZE && i<size_to_read;i++){
+        for(i=*offset;i<strlen(DISGUISE_PATTERN) && i<size_to_read;i++){
             if(i == 9){
-                /*char *randomClass = 0;
-                while(*randomClass < 1 || *randomClass > 9){
-                    get_random_bytes(randomClass,sizeof(char));
+                char randomClass = 0;
+                while(randomClass < 1 || randomClass > 9){
+                    get_random_bytes(&randomClass,sizeof(char));
                 }
-                *randomClass = *randomClass + 48;
-                user_buffer[i] = *randomClass;*/
-                user_buffer[i] = DISGUISE_PATTERN[i];
+                randomClass = randomClass + 48;
+                user_buffer[i] = randomClass;
             } else {
                 user_buffer[i] = DISGUISE_PATTERN[i];
             }
@@ -49,11 +47,6 @@ struct cdev cdev_cfg = {
 int majorNumber = -1;
 struct class* cls;
 
-static char* disg_devnode(struct device* dev, umode_t* mode){
-    *mode = 0444;
-    return NULL;
-}
-
 
 static int __init init_disg_module(void){
     int regResult;
@@ -67,7 +60,6 @@ static int __init init_disg_module(void){
     }
     majorNumber = regResult;
     cls = class_create(THIS_MODULE,"tf2d");
-    cls->devnode = disg_devnode;
     cdev_init(&cdev_cfg,&fops);
     cdev_add(&cdev_cfg,0,1);
     device_create(cls,NULL,MKDEV(majorNumber,0),NULL,"tf2d");
